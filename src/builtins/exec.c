3a870ee20e24e1d7844c037698ec49ae008c6f5a
Loic COZDENMAT <lcozdenm@e2r3p1.clusters.42paris.fr>
2023-05-01 13:27:03 +0200

    now correctly work with piped commands, [BUG] not working correctly if the command piped doesn't exist

--- src/builtins/exec.c
+++ src/builtins/exec.c
@@ -6,49 +6,119 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 13:42:56 by lcozdenm          #+#    #+#             */
-/*   Updated: 2023/04/29 19:24:51 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2023/05/01 13:18:11 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "exec.h"
+#include "error.h"
 
-static int	repipe(t_arg *out, t_arg *in, int *pip);
- 
-int	execute(t_arg	*args)
+static t_error	treat_cmds(t_cmd *cmds);
+static t_error	create_pipe(t_cmd *cmds);
+static void		close_pipe(t_cmd *cmds, int n);
+
+/// @brief  Execute each commands 1 by 1
+/// @param  cmds linked list of commands
+/// @return error nb 
+int	execute(t_cmd	*cmds)
 {
 	const char	*fname;
-	int	pip[2];
+	t_error		err;
+	t_cmd		*curr;
 
-	if (args->ac < 1)
-		return (1); //TODO, renvoyé l'erreur avec un type t_error?
-	while (args != NULL)
+	err = create_pipe(cmds);
+	if (err)
+		return (err);
+	curr = cmds;
+	while (curr != NULL)
 	{
-		pipe(pip);
-		fname = args->av[0];
-		child(args);
-		args = args->next;
+		if (curr->ac < 1)
+			return (ERR_CMD_NOARG);
+		fname = curr->av[0];
+		err = treat_cmds(curr);
+		if (err)
+		{
+			return (err);
 		}
+		curr = curr->next;
 	}
-	while(wait(NULL) > 0);
+	while (wait(NULL) > 0)
+		;
+	close_pipe(cmds, -1);
 	return (0);
 }
 
-static void	child(t_args *cmd)
+/// @brief  Execute the first command,  if there's another command in cmds,
+///         pipe its output to the input of the next one.
+/// @param  cmds linked list of commands
+/// @return error nb 
+/// @todo   check for builtin
+static t_error	treat_cmds(t_cmd *cmds)
 {
 	pid_t	pid;
+	t_cmd	*cmd;
 
+	cmd = cmds;
 	pid = fork();
 	if (pid == F_CHILD)
 	{
-		if (cmd->prev == NULL)
+		if (cmd->prev != NULL)
 		{
-			dupinout(cmd->pipe[0], cmd->pipe[1]);//check
+			if (dup2(cmd->pipe[0], STDIN) < 0)
+				return (ERR_CMD_DUP0);
+			close(cmd->pipe[0]);
 		}
+		if (cmd->next != NULL)
+		{
+			if (dup2(cmd->next->pipe[1], STDOUT) < 0)
+				return (ERR_CMD_DUP1);
+			close(cmd->next->pipe[1]);
+		}
+		execve(cmd->av[0], cmd->av, NULL);
+		return (ERR_CMD_FAIL);
 	}
+	return (GOOD);
 }
-static void	dupinout(int in, int out)
+
+/// @brief  Pipe each command
+/// @param  cmds linked list of commands
+/// @return error nb 
+static t_error	create_pipe(t_cmd *cmds)
 {
-	dup2(in, STDIN);
-	dup2(out, STDOUT);
+	t_cmd	*current;
+	int		i;
+
+	i = 0;
+	current = cmds;
+	while (current != NULL)
+	{
+		if (pipe(current->pipe) < 0)
+		{
+			close_pipe(cmds, i);
+			return (ERR_CMD_PIPE);
+		}
+		current = current->next;
+		i++;
+	}
+	return (GOOD);
+}
+
+/// @brief  Close pipes in the range of n
+/// @param  cmds linked list of commands
+/// @param  n    range, max if negative  
+static void	close_pipe(t_cmd *cmds, int n)
+{
+	int		i;
+	t_cmd	*current;
+
+	i = 0;
+	current = cmds;
+	while ((n > 0 && i < n) && current != NULL)
+	{
+		close(current->pipe[0]);
+		close(current->pipe[1]);
+		i++;
+		current = current->next;
+	}
 }
--- src/minishell/main.c
+++ src/minishell/main.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 13:30:27 by lcozdenm          #+#    #+#             */
-/*   Updated: 2023/04/29 18:42:32 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2023/05/01 13:15:30 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,24 @@ int	main(int argc, char **argv)
 		NULL,
 	};
 
-	t_arg *arg1;
-	t_arg *arg2;
-	arg2 = malloc(sizeof(t_arg));
+	t_cmd *arg1;
+	t_cmd *arg2;
+	arg2 = malloc(sizeof(t_cmd));
 	if (!arg2)
 		return (0);
 	arg2->ac = 2;
 	arg2->av = av_test2;
 	arg2->next = NULL;
-	arg2->is_piped = 0;
-	arg2->prev = arg1;
-	arg2->pipe[0] = STDIN;
-	arg2->pipe[1] = STDOUT;
-	arg1 = malloc(sizeof(t_arg));
+	arg1 = malloc(sizeof(t_cmd));
 	if (!arg1)
 		return (0);
 	arg1->ac = 2;
 	arg1->av = av_test;
 	arg1->next = arg2;
-	arg1->is_piped = 0;
 	arg1->prev = NULL;
-	arg1->pipe[0] = STDIN;
-	arg1->pipe[1] = STDOUT;
-
+	arg2->prev = arg1;
 	execute(arg1);
+	free(arg1);
+	free(arg2);
 	return (0);
 }
