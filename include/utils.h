633d8ea553c4f1323b410871d4840cc6825818e1
Moutard <loic.cozdenmat@gmail.com>
2023-05-09 13:23:37 +0200

    misc

--- include/utils.h
+++ include/utils.h
@@ -3,18 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   utils.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coltcivers <coltcivers@student.42.fr>      +#+  +:+       +#+        */
+/*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 15:13:05 by coltcivers        #+#    #+#             */
-/*   Updated: 2023/05/02 14:51:37 by coltcivers       ###   ########.fr       */
+/*   Updated: 2023/05/08 16:36:49 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PARSING_H
-# define PARSING_H
+#ifndef UTILS_H
+# define UTILS_H
 
 # include "minishell.h"
 
 void	safe_free(void *ptr);
-
 #endif
