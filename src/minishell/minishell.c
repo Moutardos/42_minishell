9a050f2a246f3b42cedc31c95bfeff713648a13a
Loic COZDENMAT <lcozdenm@e2r10p1.clusters.42paris.fr>
2023-05-15 16:35:39 +0200

    prototype free cmds et mini

--- src/minishell/minishell.c
+++ src/minishell/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: coltcivers <coltcivers@student.42.fr>      +#+  +:+       +#+        */
+/*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 19:37:11 by coltcivers        #+#    #+#             */
-/*   Updated: 2023/04/28 15:13:58 by coltcivers       ###   ########.fr       */
+/*   Updated: 2023/05/15 16:35:21 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,45 @@ t_minishell *init_minishell()
 	return (mini);
 }
 
-
-int main(int ac, char **av)
+void	free_cmds(t_cmd *cmds)
 {
-	t_minishell	*mini;
-
-	mini = init_minishell();
-	while (mini->exit == 0)
+	t_cmd	*curr;
+	curr = cmds;
+	while (curr != NULL)
 	{
-		parse_current_cmd(mini);
+		//ft_free_split(curr->av);
+		safe_free(curr->path);
+		safe_free(curr->delim_f);
+		safe_free(curr->delim);
+		if (curr->next == NULL)
+		{
+			safe_free(curr);
+			curr = NULL;
+		}
+		else
+		{
+			curr = curr->next;
+			safe_free(curr->prev);
+		}
 	}
 }
+
+void	free_mini(t_cmd *mini)
+{
+	free_cmds(mini->cmds);
+	ft_free_split(mini->paths);
+	if (env != NULL)
+		ft_lstclear(mini->env, free);
+	free(mini);
+}
+
+// int main(int ac, char **av)
+// {
+// 	t_minishell	*mini;
+
+// 	mini = init_minishell();
+// 	while (mini->exit == 0)
+// 	{
+// 		parse_current_cmd(mini);
+// 	}
+// }
