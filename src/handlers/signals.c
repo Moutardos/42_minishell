72368fa4f7ad1df6b640051f1793d5e7b8ebf298
lcozdenm <loic.cozdenmat@gmail.com>
2023-06-28 13:52:53 +0200

    no nl signal

--- src/handlers/signals.c
+++ src/handlers/signals.c
@@ -6,7 +6,7 @@
 /*   By: lcozdenm <lcozdenm@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 14:56:07 by coltcivers        #+#    #+#             */
-/*   Updated: 2023/06/28 13:47:14 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2023/06/28 13:51:48 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,9 +37,9 @@ void	setup_signals(void handler(int))
 
 void	replace_line(void)
 {
-	ft_printf("\n");
 	if (waitpid(-1, NULL, WNOHANG) == 0)
-		return ;
+	 	return ;
+	ft_printf("\n");
 	rl_on_new_line();
 	rl_replace_line("", 0);
 	rl_redisplay();
